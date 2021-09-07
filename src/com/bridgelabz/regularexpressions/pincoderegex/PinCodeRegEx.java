aaad009365f2f2dd6e211545a86e87f952a09953
padmini v <padmini.v@perfios.com>
2021-09-07 18:33:06 +0530

    ADDED: Welcome message in main method

---
+++ src/com/bridgelabz/regularexpressions/pincoderegex/PinCodeRegEx.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.regularexpressions.pincoderegex;
+
+public class PinCodeRegEx 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**Welcome to Pincode Evaluator**");
+		
+	}
+
+}
