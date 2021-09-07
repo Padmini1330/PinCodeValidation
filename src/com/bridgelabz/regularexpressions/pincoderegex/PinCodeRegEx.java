063fd55df3494d92312dbcab826dcb82036aed74
padmini v <padmini.v@perfios.com>
2021-09-07 18:39:34 +0530

    ADDED: pinCodeValidator method to validate a 6-digit pincode

--- src/com/bridgelabz/regularexpressions/pincoderegex/PinCodeRegEx.java
+++ src/com/bridgelabz/regularexpressions/pincoderegex/PinCodeRegEx.java
@@ -1,12 +1,28 @@
 package com.bridgelabz.regularexpressions.pincoderegex;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class PinCodeRegEx 
 {
+
+	public static boolean pinCodeValidator(String pinCode) 
+	{
+		Pattern pattern=Pattern.compile("[0-9]{6}$");
+		Matcher matcher=pattern.matcher(pinCode);
+		return matcher.matches();
+	}
 
 	public static void main(String[] args) 
 	{
 		System.out.println("**Welcome to Pincode Evaluator**");
-		
+		String pinCode="400088";
+		boolean isPinCode=pinCodeValidator(pinCode);
+		if(isPinCode)
+			System.out.println("Entered pincode is VALID");
+		else
+			System.out.println("Entered pincode is NOT VALID");
+
 	}
 
 }
