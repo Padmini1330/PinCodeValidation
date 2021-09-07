c703f7abebb197891cc8bb452824ccba2e806367
padmini v <padmini.v@perfios.com>
2021-09-07 19:19:48 +0530

    ADDED: Welcome message in main method

---
+++ src/com/bridgelabz/regularexpressions/emailvalidator/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.regularexpressions.emailvalidator;
+
+public class EmailValidator 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**Welcome to Email Validator**");
+
+	}
+
+}
